a18b3b1299d44a4afe027268c85c9fad1547f164
Johannes Förstner <johannes@famfoerstner.de>
2024-07-29 19:22:28 +0200

    Mood, north, lighting

--- build.bat
+++ build.bat
@@ -30,5 +30,11 @@ popd
 "%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
 if %errorlevel% neq 0 pause
 
+:: Compile resource file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%bits%\world\global\moods\%map%" "%tmp%\Bits\world\global\moods\%map%" /E
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\Resources\%map_cs%.dsres" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
+if %errorlevel% neq 0 pause
+
 :: Cleanup
 rmdir /S /Q "%tmp%\Bits"
--- run.bat
+++ run.bat
@@ -14,7 +14,7 @@ call "%bits%\build.bat" %*
 ::pause
 
 :: Run it!
-"%ds%\DungeonSiege.exe" nointro=true map=%map%
+"%ds%\DSLOA.exe" nointro=true map=%map%
 
 :: Cleanup resources so as not to confuse Siege Editor
 call "%bits%\cleanup.bat" %*
